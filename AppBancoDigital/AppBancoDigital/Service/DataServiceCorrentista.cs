ef5c584ba903159fc1089e409c5d9f6ba574a150
marianepagagnotti <marianepagagnotti.sl@gmail.com>
2023-04-25 15:44:23 -0300

    first commit

---
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -0,0 +1,57 @@
+﻿using AppBancoDigital.Model;
+using AppBancoDigital.Service;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppBancoDigital.Service
+{
+    public class DataServiceCorrentista : DataService
+    {
+        
+        public static async Task<List<Correntista>> GetPessoasAsync()
+        {
+            string json = await DataService.GetDataFromService("/correntista");
+
+            List<Correntista> arr_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return arr_correntista;
+        }
+
+        
+        public static async Task<Correntista> Cadastrar(Correntista c)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(c);
+
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/salvar");
+
+            Correntista p = JsonConvert.DeserializeObject<Correntista>(json);
+
+            return p;
+        }
+
+        public static async Task<List<Correntista>> SearchAsync(string q)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(q);
+
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/buscar");
+
+            List<Correntista> arr_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return arr_correntista;
+        }
+
+        public static async Task<List<Correntista>> DeleteAsync(int id)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(id);
+
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/delete");
+
+            List<Correntista> arr_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return arr_correntista;
+        }
+
+    }
+}
---
+++ AppBancoDigital/AppBancoDigital/View/Cadastro.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro : ContentPage
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpck_datanasc_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+    }
+}
